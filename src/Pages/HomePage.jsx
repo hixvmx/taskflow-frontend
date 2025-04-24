b73f805ee50b32342a1373f08c2ee544c57fd70e
hixvm <hixvmx@gmail.com>
2025-04-24 16:23:55 +0100

    feat: install and configure Tailwind CSS

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@
 export default function HomePage() {
     return (
         <div className="">
-            <h1 className="bg-red-300 text-3xl underline text-white">
+            <h1 className="text-3xl underline">
                 Hello world
             </h1>
         </div>
