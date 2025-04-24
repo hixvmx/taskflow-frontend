20f000fcba3488a1919b81e2822b7bda7c8f22b9
hixvm <hixvmx@gmail.com>
2025-04-24 19:51:09 +0100

    feat: integrate API to fetch all categories

--- src/components/TasksBoard/TasksList/index.jsx
+++ src/components/TasksBoard/TasksList/index.jsx
@@ -1,203 +1,52 @@
+import { useEffect, useState } from "react";
 import Category from "./Category";
+import axiosClient from "../../../lib/axiosClient";
+
 
 export default function TasksList() {
 
-    const tasks = [
-        {
-            id: 15,
-            name: "To-Do",
-            tasks: [
-                {
-                    id: 15,
-                    title: 'Implement authentication',
-                    desc: 'Set up user authentication and authorization',
-                    tags: '',
-                },
-                {
-                    id: 25,
-                    title: 'Design system components',
-                    desc: 'Create reusable UI components for the application',
-                    tags: '',
-                },
-                {
-                    id: 35,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-                {
-                    id: 45,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-            ],
-        },
-        {
-            id: 25,
-            name: "Doing",
-            tasks: [
-                {
-                    id: 55,
-                    title: 'Implement authentication',
-                    desc: 'Set up user authentication and authorization',
-                    tags: '',
-                },
-                {
-                    id: 65,
-                    title: 'Design system components',
-                    desc: 'Create reusable UI components for the application',
-                    tags: '',
-                },
-                {
-                    id: 75,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-                {
-                    id: 85,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-            ],
-        },
-        {
-            id: 15,
-            name: "To-Do",
-            tasks: [
-                {
-                    id: 15,
-                    title: 'Implement authentication',
-                    desc: 'Set up user authentication and authorization',
-                    tags: '',
-                },
-                {
-                    id: 25,
-                    title: 'Design system components',
-                    desc: 'Create reusable UI components for the application',
-                    tags: '',
-                },
-                {
-                    id: 35,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-                {
-                    id: 45,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-            ],
-        },
-        {
-            id: 25,
-            name: "Doing",
-            tasks: [
-                {
-                    id: 55,
-                    title: 'Implement authentication',
-                    desc: 'Set up user authentication and authorization',
-                    tags: '',
-                },
-                {
-                    id: 65,
-                    title: 'Design system components',
-                    desc: 'Create reusable UI components for the application',
-                    tags: '',
-                },
-                {
-                    id: 75,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-                {
-                    id: 85,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-            ],
-        },
-        {
-            id: 15,
-            name: "To-Do",
-            tasks: [
-                {
-                    id: 15,
-                    title: 'Implement authentication',
-                    desc: 'Set up user authentication and authorization',
-                    tags: '',
-                },
-                {
-                    id: 25,
-                    title: 'Design system components',
-                    desc: 'Create reusable UI components for the application',
-                    tags: '',
-                },
-                {
-                    id: 35,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-                {
-                    id: 45,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-            ],
-        },
-        {
-            id: 25,
-            name: "Doing",
-            tasks: [
-                {
-                    id: 55,
-                    title: 'Implement authentication',
-                    desc: 'Set up user authentication and authorization',
-                    tags: '',
-                },
-                {
-                    id: 65,
-                    title: 'Design system components',
-                    desc: 'Create reusable UI components for the application',
-                    tags: '',
-                },
-                {
-                    id: 75,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-                {
-                    id: 85,
-                    title: 'Design Home Page',
-                    desc: '',
-                    tags: '',
-                },
-            ],
-        },
-    ];
+    const [categories, setCategories] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    const getAllCategories = async () => {
+        try {
+            setIsLoading(true)
+
+
+            const response = await axiosClient.get('/api/categories');
+            setCategories(response.data ?? []);
+
+        } catch (err) {
+            console.log("Error:", err);
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getAllCategories();
+    }, [])
 
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-16">
-            <div className="w-full flex items-start space-x-4 overflow-x-auto pb-4">
-                {!tasks ? 'Not Found!' : (
-                    tasks?.map((i) => <Category key={i.id} name={i.name} tasks={i.tasks} />)
-                )}
-
-                <div className="group w-72 max-w-72 min-w-72">
-                    <button className="w-full text-left bg-gray-300 hover:bg-gray-400 rounded shadow-sm hover:shadow cursor-pointer p-3">
-                        <div className="text-sm font-medium text-gray-900">Add New Category</div>
-                    </button>
+            {isLoading ? suspense() :
+                <div className="w-full flex items-start space-x-4 overflow-x-auto pb-4">
+                    {categories?.map(c => <Category key={c.slug} name={c.name} tasks={c.tasks} />)}
+                    <div className="group w-72 max-w-72 min-w-72">
+                        <button className="w-full text-left bg-gray-300 hover:bg-gray-400 rounded shadow-sm hover:shadow cursor-pointer p-3">
+                            <div className="text-sm font-medium text-gray-900">Add New Category</div>
+                        </button>
+                    </div>
                 </div>
-            </div>
+            }
+        </div>
+    )
+}
+
+const suspense = () => {
+    return (
+        <div className="group w-72 max-w-72 min-w-72">
+            <p>Loading categories...</p>
         </div>
     )
 }
